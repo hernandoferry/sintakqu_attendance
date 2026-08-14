003449aefa6bad402870605cdbdc3012c116d6e1
hernando_ferry <hernando.ferry@gmail.com>
2026-08-14 10:42:40 +0700

    add: splash screen page

---
+++ lib/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Color(0xFFB2C5FF), Color(0xFFF3F6FD), Color(0xFFE1E2E4)],
+            stops: [0.0, 0.5, 1.0],
+          ),
+        ),
+
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsetsGeometry.only(top: 200, left: 57, right: 57),
+                child: Image.asset(
+                  'assets/images/sintakqu-attendance-logo.png',
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+              SizedBox(height: 415),
+              Text(
+                'Version : 0.0.1',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color(0xFF737685),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Secure Check-in',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF737685),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
